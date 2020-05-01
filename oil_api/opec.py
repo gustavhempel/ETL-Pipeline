cdff7ee2fcc3710522dc83ff617a0adb85389540
gustavhempel <t.mahn@oyacapitalinc.com>
2020-05-01 17:28:42 -0400

    continued progress implemented all pulling mechanisms v1.3

---
+++ oil_api/opec.py
@@ -0,0 +1,74 @@
+import datetime
+
+from dataflows import Flow, PackageWrapper, ResourceWrapper, validate, filter_rows
+from dataflows import add_metadata, dump_to_path, load, set_type, printer
+
+
+def rename_resources(package: PackageWrapper):
+    package.pkg.descriptor['resources'][0]['name'] = 'opec_supply_demand'
+    package.pkg.descriptor['resources'][0]['path'] = 'data/opec_supply_demand.csv'
+    package.pkg.descriptor['resources'][1]['name'] = 'opec_storage_water'
+    package.pkg.descriptor['resources'][1]['path'] = 'data/opec_storage_water.csv'
+    package.pkg.descriptor['resources'][2]['name'] = 'oil_liquid_production'
+    package.pkg.descriptor['resources'][2]['path'] = 'data/oil_liquid_production.csv'
+    package.pkg.descriptor['resources'][3]['name'] = 'glb_rig_count'
+    package.pkg.descriptor['resources'][3]['path'] = 'data/glb_rig_count.csv'
+
+    yield package.pkg
+    res_iter = iter(package)
+    for res in  res_iter:
+        yield res.it
+    yield from package
+
+
+
+oil_prices = Flow(
+    add_metadata(
+        name="Opec_data",
+        title= "A source of Opec Data",
+        descriptor="A variety of temporal granularities for Europe Brent and WTI (West Texas Intermediate) Spot Prices.",
+        sources=[
+            {
+                "name": "OPEC: Monthly Oil Market Report",
+                "path": "https://www.opec.org/opec_web/static_files_project/media/downloads/publications/MOMR%20Appendix%20Tables%20(April%202020).xlsx",
+                "title": "OPEC: Monthly Oil Market Report"
+            },
+        ]
+    ),
+    load(
+        load_source='https://www.opec.org/opec_web/static_files_project/media/downloads/publications/MOMR%20Appendix%20Tables%20(April%202020).xlsx',
+        format='xls',
+        sheet=2,
+        skip_rows=[1,2,3,4,5],
+        headers=[' ','Country', '2016','2017','2018','1Q19','2Q19','3Q19','4Q19','2019','1Q20','2Q20','3Q20','4Q20','2020']
+    ),
+    load(
+        load_source='https://www.opec.org/opec_web/static_files_project/media/downloads/publications/MOMR%20Appendix%20Tables%20(April%202020).xlsx',
+        format='xls',
+        sheet=4,
+        skip_rows=[1,2,3,4,5],
+        headers=[' ','_','Country','2017','2018','2019','-','Q417','1Q18','2Q18','3Q18','4Q18','1Q19','2Q19','3Q19','4Q19']
+    ),
+    load(
+        load_source='https://www.opec.org/opec_web/static_files_project/media/downloads/publications/MOMR%20Appendix%20Tables%20(April%202020).xlsx',
+        format='xls',
+        sheet=5,
+        skip_rows=[1,2,3,4,5,6],
+        headers=[' ','Country','2016','2017','2018','3Q19','4Q19','2019','Change 19/18','1Q20','2Q20','3Q20','4Q20','Change 20/19']
+    ),
+    load(
+        load_source='https://www.opec.org/opec_web/static_files_project/media/downloads/publications/MOMR%20Appendix%20Tables%20(April%202020).xlsx',
+        format='xls',
+        sheet=6,
+        skip_rows=[1,2,3,4,5,6],
+        headers=[' ','Country','2017','2018','2019','Change 19/20','2Q19','3Q19','4Q19','1Q20','Feb20','Mar20','Change Mar/Feb']
+    ),
+    rename_resources,
+    validate(),
+    printer(),
+    dump_to_path('opec'),
+)
+
+
+oil_prices.process()
+
--- oil_api/pull.py
+++ oil_api/pull.py
@@ -7,20 +7,9 @@ from dataflows import add_metadata, dump_to_path, load, set_type, printer
 def rename_resources(package: PackageWrapper):
     package.pkg.descriptor['resources'][0]['name'] = 'brent-daily'
     package.pkg.descriptor['resources'][0]['path'] = 'oil_api/data/brent_daily.csv'
-    package.pkg.descriptor['resources'][1]['name'] = 'brent-week'
-    package.pkg.descriptor['resources'][1]['path'] = 'oil_api/data/brent_weekly.csv'
-    package.pkg.descriptor['resources'][2]['name'] = 'brent-month'
-    package.pkg.descriptor['resources'][2]['path'] = 'oil_api/data/brent_monthly.csv'
-    package.pkg.descriptor['resources'][3]['name'] = 'brent-year'
-    package.pkg.descriptor['resources'][3]['path'] = 'oil_api/data/brent_yearly.csv'
-    package.pkg.descriptor['resources'][4]['name'] = 'wti-daily'
-    package.pkg.descriptor['resources'][4]['path'] = 'oil_api/data/wti_daily.csv'
-    package.pkg.descriptor['resources'][5]['name'] = 'wti-week'
-    package.pkg.descriptor['resources'][5]['path'] = 'oil_api/data/wti_weekly.csv'
-    package.pkg.descriptor['resources'][6]['name'] = 'wti-month'
-    package.pkg.descriptor['resources'][6]['path'] = 'oil_api/data/wti_monthly.csv'
-    package.pkg.descriptor['resources'][7]['name'] = 'wti-year'
-    package.pkg.descriptor['resources'][7]['path'] = 'oil_api/data/wti_yearly.csv'
+    package.pkg.descriptor['resources'][1]['name'] = 'wti-daily'
+    package.pkg.descriptor['resources'][1]['path'] = 'oil_api/data/wti_daily.csv'
+
 
     yield package.pkg
     res_iter = iter(package)
@@ -47,40 +36,15 @@ oil_prices = Flow(
                 "title": "Daily Europe Brent Spot Price"
             },
             {
-                "name": "Weekly Europe Brent Spot Price",
-                "path": "https://www.eia.gov/dnav/pet/hist_xls/RBRTEw.xls",
-                "title": "Weekly Europe Brent Spot Price"
-            },
-            {
-                "name": "Monthly Europe Brent Spot Price",
-                "path": "https://www.eia.gov/dnav/pet/hist_xls/RBRTEm.xls",
-                "title": "Monthly Europe Brent Spot Price"
-            },
-            {
-                "name": "Annual Europe Brent Spot Price",
-                "path": "https://www.eia.gov/dnav/pet/hist_xls/RBRTEa.xls",
-                "title": "Annual Europe Brent Spot Price"
-            },
-            {
                 "name": "Daily Cushing, OK WTI Spot Price",
                 "path": "http://www.eia.gov/dnav/pet/hist_xls/RWTCd.xls",
                 "title": "Daily Cushing, OK WTI Spot Price"
             },
             {
-                "name": "Weekly Cushing, OK WTI Spot Price",
-                "path": "http://www.eia.gov/dnav/pet/hist_xls/RWTCw.xls",
-                "title": "Weekly Cushing, OK WTI Spot Price"
+                "name": "OPEC: Monthly Oil Market Report",
+                "path": "https://www.opec.org/opec_web/static_files_project/media/downloads/publications/MOMR%20Appendix%20Tables%20(April%202020).xlsx",
+                "title": "OPEC: Monthly Oil Market Report"
             },
-            {
-                "name": "Monthly Cushing, OK WTI Spot Price",
-                "path": "http://www.eia.gov/dnav/pet/hist_xls/RWTCm.xls",
-                "title": "Monthly Cushing, OK WTI Spot Price"
-            },
-            {
-                "name": "Annual Cushing, OK WTI Spot Price",
-                "path": "http://www.eia.gov/dnav/pet/hist_xls/RWTCa.xls",
-                "title": "Annual Cushing, OK WTI Spot Price"
-            }
         ],
         licenses=[
             {
@@ -112,62 +76,21 @@ oil_prices = Flow(
         headers=['Date', 'Price']
     ),
     load(
-        load_source='https://www.eia.gov/dnav/pet/hist_xls/RBRTEw.xls',
-        format='xls',
-        sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
-    ),
-    load(
-        load_source='https://www.eia.gov/dnav/pet/hist_xls/RBRTEm.xls',
-        format='xls',
-        sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
-    ),
-    load(
-        load_source='https://www.eia.gov/dnav/pet/hist_xls/RBRTEa.xls',
-        format='xls',
-        sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
-    ),
-    load(
         load_source='http://www.eia.gov/dnav/pet/hist_xls/RWTCd.xls',
         format='xls',
         sheet=2,
         skip_rows=[1,2,3],
         headers=['Date', 'Price']
     ),
-    load(
-        load_source='http://www.eia.gov/dnav/pet/hist_xls/RWTCw.xls',
-        format='xls',
-        sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
-    ),
-    load(
-        load_source='http://www.eia.gov/dnav/pet/hist_xls/RWTCm.xls',
-        format='xls',
-        sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
-    ),
-    load(
-        load_source='http://www.eia.gov/dnav/pet/hist_xls/RWTCa.xls',
-        format='xls',
-        sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
-    ),
+    
+    
     rename_resources,
     set_type('Date', resources=None, type='date', format='any'),
     validate(),
     printer(),
     filter_out_empty_rows,
-    dump_to_path(),
+    dump_to_path('prices'),
 )
 
 
-if __name__ == 'pull':
-    oil_prices.process()
+oil_prices.process()
